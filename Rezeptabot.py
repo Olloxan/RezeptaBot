911138bdc0b45a235ecc95c50d587935e44c10ba
Ollox <axe999@gmx.de>
2024-10-26 15:44:22 +0200

    erster Test mit RunnableBranch. Nur theorie bisher

--- Rezeptabot.py
+++ Rezeptabot.py
@@ -4,6 +4,9 @@ from UserInterface import UserInterface
 from VectorStore import VectorStore
 from Chatbot import ChatbotWithHistory
 
+
+from langchain_core.runnables import RunnableLambda, RunnableAssign, RunnableBranch
+
 modelname = "llama3.1:8b-instruct-q4_K_S"
 model = Ollama(model = modelname)
 
@@ -17,29 +20,46 @@ conversation_state = 0
 
 state = {}
 
+class ConversationStateMachine:
+    def __init__(self):
+        self.state = "collect_info"  # Initial state
+    
+
+    def update_state(self, model_output):
+        # ich will, dass hier einfach nur das state_dict rein geht und der state raus geholt wird
+        if "enough information" in model_output.lower():
+            self.state = "process_info"
+        elif "processing complete" in model_output.lower():
+            self.state = "provide_summary"
+        # Add more complex logic as needed
+    
+    def get_state(self):
+        return self.state
+
+state_machine = ConversationStateMachine()
+
+branch = RunnableBranch(
+    default= model,  # Default chain to run
+    branches={
+        "collect_info": model,
+        "process_info": model,
+        "provide_summary": model,
+    },
+    select_branch=lambda inputs: state_machine.get_state()  # Dynamic state selection
+    )
+
 def chat_gen(message, history=[], return_buffer=True):        
     buffer = "" 
+    
+    output = branch.invoke(state)
+    streamoutput = output['answer']
+    for token in streamoutput:            
+        buffer += token
+        yield buffer
+    
     #### ToDo ####
-    if conversation_state == 0:
-        
-    # State 0: 
-    # Plauerphase: das Netzwerk unterhaelt sich mit dem Nutzer (ein kurzes Lebenszeichen, um zu sehen, ob Ollama laeuft, Ollama aus einem Subprocess heraus starten, wenn es nicht schon laeuft)    
-        state['message'] = message
-        state['history'] = history
-        for token in chatbot.stream(state):           
-            buffer += token
-            yield buffer if return_buffer else token
-
-    elif conversation_state == 1:
-    # state 1: 
-        docs_and_scores = chroma_db.similarity_search_with_score(query=message, k=20)
-
-        for doc, score in docs_and_scores:
-            buffer += f"Document: {doc}, Similarity Score: {score}\n"
-            yield buffer
-        
-    # state 2: Einkaufsliste
-       
+    
+     
 
     # for token in model.stream(message):        
     # ## If you're using standard print, keep line from getting too long
