215b0458e550fbf0505d024a846ff12798df052c
Ollox <axe999@gmx.de>
2024-11-26 17:32:16 +0100

    added test recipes

--- RecipePrepare.py
+++ RecipePrepare.py
@@ -10,25 +10,25 @@ llm = Ollama(model = modelname)
 
 ###### Part 1 #######
 ## load documents from json format
-# pages = load_documents_from_disk('Recipes/Json/AllRecipes.json')
-# selected_pages = [pages[i] for i in [0, 1, 2, 4, 60, 212]]
-# state = {'input': selected_pages}
+pages = load_documents_from_disk('Recipes/Json/AllRecipes.json')
+selected_pages = [pages[i] for i in [0, 1, 2, 4, 60, 212, 18, 19, 20, 21, 22, 23, 24, 25, 26 , 27, 28]]
+state = {'input': selected_pages}
 
 
-# recipeSeparator = RunnableRecipeSeparator(llm)
-# docs = recipeSeparator.invoke(state)
+recipeSeparator = RunnableRecipeSeparator(llm)
+docs = recipeSeparator.invoke(state)
 
-# store_documents_on_disk(docs, "logs/Separated.json")
+store_documents_on_disk(docs, "logs/Separated.json")
 
 ###### Part 2 #######
-# pages = load_documents_from_disk("logs/Separated.json")
-# state = {'input': pages}
+pages = load_documents_from_disk("logs/Separated.json")
+state = {'input': pages}
 
 
-# rawIngredientExtracor = RunnableRawIngredientExtracor(llm)
+rawIngredientExtracor = RunnableRawIngredientExtracor(llm)
 
-# raw_ingredients = rawIngredientExtracor.invoke(state)
-# store_raw_ingredient_lists_on_disk(raw_ingredients, 'logs/RawIngredients.json')
+raw_ingredients = rawIngredientExtracor.invoke(state)
+store_raw_ingredient_lists_on_disk(raw_ingredients, 'logs/RawIngredients.json')
 
 ###### Part 3 #######
 
