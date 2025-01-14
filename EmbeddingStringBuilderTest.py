a51432c613b53cfe9b8dd109266c6771bb1ec2f5
Ollox <axe999@gmx.de>
2025-01-14 11:12:54 +0100

    Added new Test for Embeddingstringbuilder

---
+++ EmbeddingStringBuilderTest.py
@@ -0,0 +1,10 @@
+from langchain_community.llms import Ollama
+from Runnables import RunnableEmbeddingStringBuilder
+
+class TestRunnableEmbeddingStringBuilder():
+    def __init__(self) -> None:
+        pass
+
+if __name__ == '__main__':
+    kacka = TestRunnableEmbeddingStringBuilder()
+    kacka.test_my()
